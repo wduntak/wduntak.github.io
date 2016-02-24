6d490c4e71527c851b3b450c412ed0f4293b7f09
Wangdu Duntak <wduntak@lp-229.kobo.corp>
2016-02-24 13:38:36 -0500

    Remove bar animation

--- js/ScrollAnimation.js
+++ js/ScrollAnimation.js
@@ -5,30 +5,37 @@ $(function(){
 	controller = new ScrollMagic.Controller();
 
 	//Create the Scene and trigger when visiblewith ScrollMagic
-	new ScrollMagic.Scene({triggerElement: "#work", duration: 1000, offset: 700})
+	new ScrollMagic.Scene({
+		triggerElement: "#work", 
+		duration: 1000, 
+		offset: 700
+	})
 		.setTween(TweenMax.to("#block1", 6, {top: "+=2800", width:"400", height: "400", left:"22%"}))
 		.setClassToggle("#block1", "parachute")
 		// .addIndicators()
 		.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#experience", duration: 1000})
+	new ScrollMagic.Scene({
+		triggerElement: "#experience", 
+		duration: 1000
+	})
 		.setTween(TweenMax.to('#block1', 4, {top: "+=900", width: "300", height: "300", left: "24.5%"}))
 		.setClassToggle("#block1", "parachute")
 		.on("progress", function(event){
-			if(event.progress == 1) {
-				$("#block1").addClass("bar");
-			};
+			// if(event.progress == 1) {
+			// 	$("#block1").addClass("bar");
+			// };
 		})
 		// .addIndicators()
 		.addTo(controller);
 
-	new ScrollMagic.Scene({
-		triggerElement: "#experience",
-		offset: 100,
-		duration: 900
-	})
-	.setClassToggle("#block1", "bar")
-	.addTo(controller);
+	// new ScrollMagic.Scene({
+	// 	triggerElement: "#experience",
+	// 	offset: 100,
+	// 	duration: 900
+	// })
+	// .setClassToggle("#block1", "bar")
+	// .addTo(controller);
 
 	new ScrollMagic.Scene({
 		triggerElement: "#contact",
