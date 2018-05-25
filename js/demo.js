495642da18cb087212d629a0281223a9bd3fdbab
yindayin <yindayin@gmail.com>
2018-05-24 22:00:02 -0400

    Swap out layout

---
+++ js/demo.js
@@ -0,0 +1,41 @@
+/**
+ * demo.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
+{
+	console.log("Initialize demo.js");
+	setTimeout(() => document.body.classList.add('render'), 60);
+	// const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+	// const total = navdemos.length;
+	// const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
+	// const navigate = (linkEl) => {
+	// 	console.log('navigate linkEl:', linkEl);
+	// 	document.body.classList.remove('render');
+	// 	document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+	// };
+	// navdemos.forEach(link => link.addEventListener('click', (ev) => {
+	// 	console.log("ev", ev);
+	// 	ev.preventDefault();
+	// 	navigate(ev.target);
+	// }));
+	// document.addEventListener('keydown', (ev) => {
+	// 	const keyCode = ev.keyCode || ev.which;
+	// 	let linkEl;
+	// 	if ( keyCode === 37 ) {
+	// 		linkEl = current > 0 ? navdemos[current-1] : navdemos[total-1];
+	// 	}
+	// 	else if ( keyCode === 39 ) {
+	// 		linkEl = current < total-1 ? navdemos[current+1] : navdemos[0];
+	// 	}
+	// 	else {
+	// 		return false;
+	// 	}
+	// 	navigate(linkEl);
+	// });
+}
